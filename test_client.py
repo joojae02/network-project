0f7fba4baedce2ea6a8b0fcd024813c225c8146c
wnwogus <56969647+wnwogus@users.noreply.github.com>
2023-05-26 07:47:10 +0900

    .

--- test_client.py
+++ test_client.py
@@ -166,42 +166,42 @@ class ChatClient(tk.Tk):
 
 ##################################
 
-def video_send_frames(video_client_socket):
-    while True:
-        if video_client_socket:
-            vid = cv2.VideoCapture("./test_video2.mp4")
-            while(vid.isOpened()):
-                img,frame = vid.read()
-                a = pickle.dumps(frame)
-                message = struct.pack("Q",len(a))+a
-                video_client_socket.sendall(message)
-                cv2.imshow("Client_Client",frame)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    video_client_socket.close()
-                    cv2.destroyAllWindows()
-                    break
+# def video_send_frames(video_client_socket):
+#     while True:
+#         if video_client_socket:
+#             vid = cv2.VideoCapture("./test_video2.mp4")
+#             while(vid.isOpened()):
+#                 img,frame = vid.read()
+#                 a = pickle.dumps(frame)
+#                 message = struct.pack("Q",len(a))+a
+#                 video_client_socket.sendall(message)
+#                 cv2.imshow("Client_Client",frame)
+#                 if cv2.waitKey(1) & 0xFF == ord('q'):
+#                     video_client_socket.close()
+#                     cv2.destroyAllWindows()
+#                     break
 
-def video_rev_frames(video_client_socket):
-    data = b""
-    payload_size = struct.calcsize("Q")
-    while True :
-        while len(data) < payload_size:
-            packet = video_client_socket.recv(4*1024)
-            if not packet: break
-            data+=packet
-        packed_msg_size = data[:payload_size]
-        data = data[payload_size:]
-        msg_size = struct.unpack("Q",packed_msg_size)[0]
-        while len(data) < msg_size:
-            data += video_client_socket.recv(4*1024)
-        frame_data = data[:msg_size]
-        data  = data[msg_size:]
-        frame = pickle.loads(frame_data)
-        cv2.imshow("Client_Server",frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            video_client_socket.close()
-            cv2.destroyAllWindows()
-            break
+# def video_rev_frames(video_client_socket):
+#     data = b""
+#     payload_size = struct.calcsize("Q")
+#     while True :
+#         while len(data) < payload_size:
+#             packet = video_client_socket.recv(4*1024)
+#             if not packet: break
+#             data+=packet
+#         packed_msg_size = data[:payload_size]
+#         data = data[payload_size:]
+#         msg_size = struct.unpack("Q",packed_msg_size)[0]
+#         while len(data) < msg_size:
+#             data += video_client_socket.recv(4*1024)
+#         frame_data = data[:msg_size]
+#         data  = data[msg_size:]
+#         frame = pickle.loads(frame_data)
+#         cv2.imshow("Client_Server",frame)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             video_client_socket.close()
+#             cv2.destroyAllWindows()
+#             break
 ##################################
 
 
@@ -248,16 +248,16 @@ def run_tk(root, interval=0.05):  # 50 ms
 run_tk(chat_client)
 
 
-try:
-    video_client_socket.connect((video_host_ip,video_port)) 
-    print('서버에 연결되었습니다.')
-    video_send_thread = threading.Thread(target=video_send_frames, args=(video_client_socket,))
-    video_send_thread.start()
-    video_rev_thread = threading.Thread(target=video_rev_frames, args=(video_client_socket,))
-    video_rev_thread.start()
+# try:
+#     video_client_socket.connect((video_host_ip,video_port)) 
+#     print('서버에 연결되었습니다.')
+#     video_send_thread = threading.Thread(target=video_send_frames, args=(video_client_socket,))
+#     video_send_thread.start()
+#     video_rev_thread = threading.Thread(target=video_rev_frames, args=(video_client_socket,))
+#     video_rev_thread.start()
 
-except KeyboardInterrupt:
-    print("서버 종료")
-    video_client_socket.close()
-    exit()
+# except KeyboardInterrupt:
+#     print("서버 종료")
+#     video_client_socket.close()
+#     exit()
 
--- test_server.py
+++ test_server.py
@@ -123,7 +123,7 @@ class SFTPServerUI(tk.Tk):
 def start_sftp_server():
     # RSA 키를 생성하고 SFTP 서버를 설정
     host_key = paramiko.RSAKey.generate(2048)
-    sftpserver = paramiko.Transport(('127.0.1.1', 22))
+    sftpserver = paramiko.Transport(('localhost', 22))
     sftpserver.add_server_key(host_key)
     server = SFTPServer()
 
@@ -187,41 +187,41 @@ class ChatServer(tk.Tk):
 
 ###############
 
-def video_send_frames(video_client_socket):
-    while True:
-        if video_client_socket:
-            vid = cv2.VideoCapture(0)
-            while(vid.isOpened()):
-                img,frame = vid.read()
-                a = pickle.dumps(frame)
-                message = struct.pack("Q",len(a))+a
-                video_client_socket.sendall(message)
-                cv2.imshow("Server_Client",frame)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    video_client_socket.close()
-                    cv2.destroyAllWindows()
-                    break
-def video_rev_frames(video_client_socket):
-    data = b""
-    payload_size = struct.calcsize("Q")
-    while True :
-        while len(data) < payload_size:
-            packet = video_client_socket.recv(4*1024)
-            if not packet: break
-            data+=packet
-        packed_msg_size = data[:payload_size]
-        data = data[payload_size:]
-        msg_size = struct.unpack("Q",packed_msg_size)[0]
-        while len(data) < msg_size:
-            data += video_client_socket.recv(4*1024)
-        frame_data = data[:msg_size]
-        data  = data[msg_size:]
-        frame = pickle.loads(frame_data)
-        cv2.imshow("Server_Server",frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            video_client_socket.close()
-            cv2.destroyAllWindows()
-            break
+# def video_send_frames(video_client_socket):
+#     while True:
+#         if video_client_socket:
+#             vid = cv2.VideoCapture(0)
+#             while(vid.isOpened()):
+#                 img,frame = vid.read()
+#                 a = pickle.dumps(frame)
+#                 message = struct.pack("Q",len(a))+a
+#                 video_client_socket.sendall(message)
+#                 cv2.imshow("Server_Client",frame)
+#                 if cv2.waitKey(1) & 0xFF == ord('q'):
+#                     video_client_socket.close()
+#                     cv2.destroyAllWindows()
+#                     break
+# def video_rev_frames(video_client_socket):
+#     data = b""
+#     payload_size = struct.calcsize("Q")
+#     while True :
+#         while len(data) < payload_size:
+#             packet = video_client_socket.recv(4*1024)
+#             if not packet: break
+#             data+=packet
+#         packed_msg_size = data[:payload_size]
+#         data = data[payload_size:]
+#         msg_size = struct.unpack("Q",packed_msg_size)[0]
+#         while len(data) < msg_size:
+#             data += video_client_socket.recv(4*1024)
+#         frame_data = data[:msg_size]
+#         data  = data[msg_size:]
+#         frame = pickle.loads(frame_data)
+#         cv2.imshow("Server_Server",frame)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             video_client_socket.close()
+#             cv2.destroyAllWindows()
+#             break
 
 
 ###############
@@ -263,30 +263,30 @@ loop.run_forever()
 
 
 
-video_server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-video_host_name  = socket.gethostname()
-video_host_ip = socket.gethostbyname(video_host_name)
-print('HOST IP:',video_host_ip)
-video_port = 10050
+# video_server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# video_host_name  = socket.gethostname()
+# video_host_ip = socket.gethostbyname(video_host_name)
+# print('HOST IP:',video_host_ip)
+# video_port = 10050
 
 
-socket_address = (video_host_ip,video_port)
-print('Socket created')
-video_server_socket.bind(socket_address)
-print('Socket bind complete')
-video_server_socket.listen(5)
-print('Socket now listening')
+# socket_address = (video_host_ip,video_port)
+# print('Socket created')
+# video_server_socket.bind(socket_address)
+# print('Socket bind complete')
+# video_server_socket.listen(5)
+# print('Socket now listening')
 
 
-try:
-    video_client_socket,video_addr = video_server_socket.accept()
-    print('Connection from:',video_addr)
-    video_send_thread = threading.Thread(target=video_send_frames, args=(video_client_socket,))
-    video_send_thread.start()
-    video_rev_thread = threading.Thread(target=video_rev_frames, args=(video_client_socket,))
-    video_rev_thread.start()
+# try:
+#     video_client_socket,video_addr = video_server_socket.accept()
+#     print('Connection from:',video_addr)
+#     video_send_thread = threading.Thread(target=video_send_frames, args=(video_client_socket,))
+#     video_send_thread.start()
+#     video_rev_thread = threading.Thread(target=video_rev_frames, args=(video_client_socket,))
+#     video_rev_thread.start()
     
-except KeyboardInterrupt:
-    print("서버 종료")
-    video_server_socket.close()
-    exit()
+# except KeyboardInterrupt:
+#     print("서버 종료")
+#     video_server_socket.close()
+#     exit()
